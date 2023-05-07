3481fb780aab7599d2561e17bbac884eada658ac
Hannah <76073097+hannahouazzane@users.noreply.github.com>
2023-05-07 23:32:58 +0100

    feat: remove cookie when player leaves

--- Client/my-react-app/src/ResultsPopup/ResultsPopup.js
+++ Client/my-react-app/src/ResultsPopup/ResultsPopup.js
@@ -1,12 +1,17 @@
 import "./ResultsPopup.css";
+import Cookies from "js-cookie";
 
 export const ResultsPopup = () => {
+  const removeCookie = () => {
+    Cookies.remove("player-details");
+    window.location.reload();
+  };
   return (
     <div id="pop-up" className="game-over-popup" style={{ display: "none" }}>
       <div className="popup-content">
         {" "}
         <div id="gameResult" className="popup-title"></div>
-        <button>Play another game</button>
+        <button onClick={removeCookie}> Return Home </button>
       </div>
     </div>
   );
